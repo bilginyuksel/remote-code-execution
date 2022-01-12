e1a7d1b654e3ba8293853761892b4dd034e548d7
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-01-13 02:04:42 +0300

    Refactor rc/list.go list only the containers which are created by the image

--- internal/rc/list.go
+++ internal/rc/list.go
@@ -4,19 +4,28 @@ import (
 	"context"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 )
 
-// List the containers
+// List the running containers which are created by the image
 func (c *Client) List(ctx context.Context) ([]string, error) {
-	containers, err := c.containerPort.ContainerList(ctx, types.ContainerListOptions{})
-	if err != nil {
-		return nil, err
-	}
+	containers, err := c.containerPort.ContainerList(ctx,
+		types.ContainerListOptions{
+			Filters: filters.NewArgs(
+				filters.KeyValuePair{
+					Key:   "status",
+					Value: "running",
+				},
+				filters.KeyValuePair{
+					Key:   "ancestor",
+					Value: c.containerConfig.Image,
+				}),
+		})
 
 	var containerIDs []string
 	for idx := range containers {
 		containerIDs = append(containerIDs, containers[idx].ID)
 	}
 
-	return containerIDs, nil
+	return containerIDs, err
 }
--- internal/rc/list_test.go
+++ internal/rc/list_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/codigician/remote-code-execution/internal/mocks"
 	"github.com/codigician/remote-code-execution/internal/rc"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,7 +18,7 @@ func TestList_ContainerListFailed_ReturnErr(t *testing.T) {
 	mockContainerPort := newMockContainerPort(t)
 	mockContainerPort.EXPECT().ContainerList(gomock.Any(), gomock.Any()).Return(nil, errors.New("fail"))
 
-	client := rc.NewClient(mockContainerPort, nil)
+	client := rc.NewClient(mockContainerPort, &container.Config{})
 	containers, err := client.List(context.Background())
 
 	assert.Nil(t, containers)
@@ -25,13 +27,19 @@ func TestList_ContainerListFailed_ReturnErr(t *testing.T) {
 
 func TestList_Listed_ReturnContainerIDs(t *testing.T) {
 	mockContainerPort := newMockContainerPort(t)
-	mockContainerPort.EXPECT().ContainerList(gomock.Any(), gomock.Any()).
-		Return([]types.Container{{ID: "container-1"}, {ID: "container-2"}}, nil)
+	expectedListOpts := types.ContainerListOptions{
+		Filters: filters.NewArgs(
+			filters.KeyValuePair{Key: "ancestor", Value: "my-image:latest"},
+			filters.KeyValuePair{Key: "status", Value: "running"}),
+	}
+	mockContainerPort.EXPECT().ContainerList(gomock.Any(), expectedListOpts).
+		Return([]types.Container{{ID: "container-1", Image: "my-image:latest"},
+			{ID: "container-2", Image: "my-image:latest"}}, nil)
 
-	client := rc.NewClient(mockContainerPort, nil)
+	client := rc.NewClient(mockContainerPort, &container.Config{Image: "my-image:latest"})
 	containerIDs, _ := client.List(context.Background())
 
-	assert.Equal(t, containerIDs, []string{"container-1", "container-2"})
+	assert.Equal(t, []string{"container-1", "container-2"}, containerIDs)
 }
 
 func newMockContainerPort(t *testing.T) *mocks.MockContainerPort {
