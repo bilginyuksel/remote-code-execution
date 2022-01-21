6a1145b3999f68f5d550f8ead056e640d9f94ef3
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-01-21 20:04:48 +0300

    Add create container test

--- internal/rc/integration_test.go
+++ internal/rc/integration_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/codigician/remote-code-execution/internal/rc"
 	"github.com/codigician/remote-code-execution/pkg/config"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/stretchr/testify/suite"
@@ -14,8 +15,9 @@ import (
 
 type DockerContainerClientTestSuite struct {
 	suite.Suite
-	dockerClient *client.Client
-	client       *rc.Client
+	dockerClient        *client.Client
+	client              *rc.Client
+	createdContainerIDs []string
 }
 
 func (s *DockerContainerClientTestSuite) SetupSuite() {
@@ -34,6 +36,11 @@ func (s *DockerContainerClientTestSuite) SetupSuite() {
 }
 
 func (s *DockerContainerClientTestSuite) TearDownSuite() {
+	ctx := context.Background()
+	for _, id := range s.createdContainerIDs {
+		_ = s.dockerClient.ContainerStop(ctx, id, nil)
+		_ = s.dockerClient.ContainerRemove(ctx, id, types.ContainerRemoveOptions{})
+	}
 	s.dockerClient.Close()
 }
 
@@ -44,4 +51,15 @@ func TestIntegrationRemoteContainerWithDockerClient(t *testing.T) {
 func (s *DockerContainerClientTestSuite) TestList() {
 	l, _ := s.client.List(context.Background())
 	log.Println(l)
+}
+
+func (s *DockerContainerClientTestSuite) TestCreateContainer() {
+	ctx := context.Background()
+	cid, err := s.client.Create(ctx, &container.HostConfig{})
+	s.createdContainerIDs = append(s.createdContainerIDs, cid)
+
+	clist, _ := s.dockerClient.ContainerList(ctx, types.ContainerListOptions{})
+
+	s.Nil(err)
+	s.Equal(cid, clist[0].ID)
 }
